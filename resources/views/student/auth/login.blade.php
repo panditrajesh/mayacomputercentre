e70b170aa70d1f14e393dd41a3c5abddccf90b27
Arindam <abera0275@gmail.com>
2025-08-31 00:10:01 +0530

    update

--- resources/views/student/auth/login.blade.php
+++ resources/views/student/auth/login.blade.php
@@ -1,173 +1,165 @@
-
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-        
-        <meta charset="utf-8" />
         <title>Student Login</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Themesbrand" name="author" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="../../assets/img/favicon/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="../../assets/img/favicon/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="../../assets/img/favicon/favicon-16x16.png" sizes="16x16" type="image/png">
 
-        <!-- Bootstrap Css -->
-        <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    <link rel="mask-icon" href="../../assets/img/favicon/safari-pinned-tab.svg" color="#563d7c">
+    <link rel="icon" href="../../assets/img/favicon/favicon.ico">
+    <meta name="msapplication-config" content="../../assets/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#563d7c">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    
+    <!-- Apex Charts -->
+    <link type="text/css" href="/vendor/apexcharts/apexcharts.css" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- Datepicker -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/dist/css/datepicker.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/dist/css/datepicker-bs4.min.css">
 
-    </head>
-<style type="text/css">
-     body {
-        background: url({{ asset('backend/assets/img/back.jpg') }});
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        }
-    .bg-primary.bg-soft {
-    background-color: rgb(239 128 30 / 34%)!important;
-    }
-    .card {
-    background-color: #213d941c;
-    -webkit-box-shadow: 0 0.75rem 1.5rem rgb(18 38 63 / 3%);
-    box-shadow: 0 0.75rem 1.5rem rgb(79 101 170 / 47%);
-    }
-    .clr{
-        background:white;
-    }
-</style>
-    <body>
-        <div class="account-pages my-5 pt-sm-4">
-            <div class="container">
-                <div class="row ">
-                    <!--<div class="col-md-3 col-lg-3 col-xl-3"></div>-->
-                    <div class="col-md-6 col-lg-6 col-xl-5">
-                        <div class="card overflow-hidden">
-                            <div class="bg-primary bg-soft">
-                                <div class="row">
-                                    <div class="col-7">
-                                        <div class="text-dark p-4">
-                                            <!--<h5 class="text-primary text-light">Welcome Back !</h5>-->
-                                            <h5 class="text-dark">Maya Computer Center</h5>
-                                        </div>
-                                    </div>
-                                    <div class="col-5 align-self-end">
-                                        <img src="assets/images/profile-img.png" alt="" class="img-fluid">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body pt-0"> 
-                                <div class="auth-logo">
-                                    <a href="javascript:void(0);" class="auth-logo-light">
-                                        <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="assets/images/logo-light.svg" alt="" class="rounded-circle" height="75">
-                                            </span>
-                                        </div>
-                                    </a>
+    <!-- Fontawesome -->
+    <link type="text/css" href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    
+    <!-- Sweet Alert -->
+    <link type="text/css" href="/vendor/sweetalert2/sweetalert2.min.css" rel="stylesheet">
+    
+    <!-- Notyf -->
+    <link type="text/css" href="/vendor/notyf/notyf.min.css" rel="stylesheet">
+    
+    <!-- Volt CSS -->
+    <link type="text/css" href="{{ asset('public/css/volt.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-                                    <a href="index" class="auth-logo-dark">
-                                        <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="assets/images/apprise.jpg" alt="" class="rounded-circle"  height"82px" width ="84px">
-                                            </span>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="p-2">
-                                    @if(Session::has('error'))
-                                    <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('error') }}</p>
-                                    @endif
-                                    <form class="form-horizontal" method="post">
-                                        @csrf
-                                        <div class="mb-3">
-                                            <label for="user_name" class="form-label">Registration No</label>
-                                            <input type="text" class="form-control" name="reg_no" placeholder="Enter username" minlength='3' required>
-                                        </div>
-                
-                                        <div class="mb-4">
-                                            <label class="form-label">Password</label>
-                                            <div class="input-group auth-pass-inputgroup">
-                                                <input type="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon" minlength='3' name ="mobile" required>
-                                                <button class="btn btn-light " type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
-                                            </div>
-                                        </div>
-<!-- 
-                                        <div class="form-check">
-                                            <button class="btn btn-sm btn-info"> Login With OTP</button>
-                                               
-                                        </div> -->
-                                        
-                                        <div class="mt-3 d-grid ">
-                                            <button class="btn btn-primary waves-effect waves-light" type="submit" id="login_btn">Log In</button>
-                                        </div>
-            
-                                         <!--<div class="mt-4 text-center">-->
-                                         <!--   <h5 class="font-size-14 mb-3">Login with</h5>-->
-            
-                                            <!--<ul class="list-inline">-->
-                                                <!--<li class="list-inline-item">-->
-                                                <!--    <a href="otp" class="social-list-item bg-primary text-white border-primary" title="Login With OTP">-->
-                                                <!--        <i class="fa fa-mobile"></i>-->
-                                                <!--    </a>-->
-                                                <!--</li>-->
-                                                <!--<li class="list-inline-item">-->
-                                                <!--    <a href="javascript::void()" class="social-list-item bg-info text-white border-info" title="Login With QRCode">-->
-                                                <!--        <i class="fa fa-qrcode"></i>-->
-                                                <!--    </a>-->
-                                                <!--</li>-->
-                                                <!-- <li class="list-inline-item">
-                                                    <a href="javascript::void()" class="social-list-item bg-danger text-white border-danger">
-                                                        <i class="mdi mdi-google"></i>
-                                                    </a>
-                                                </li> -->
-                                            <!--</ul>-->
-                                        <!--</div>-->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" />
 
-                                        <!-- <div class="mt-4 text-center">
-                                            <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
-                                        </div> -->
-                                    </form>
-                                <!-- </div>  -->
-            
-                            </div>
-                        </div>
-                       <!--  <div class="mt-5 text-center">
+
+    <!-- Core -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
+    <!-- Vendor JS -->
+    <script src="/assets/js/on-screen.umd.min.js"></script>
+
+    <!-- Slider -->
+    <script src="/assets/js/nouislider.min.js"></script>
+
+    <!-- Smooth scroll -->
+    <script src="/assets/js/smooth-scroll.polyfills.min.js"></script>
+
+    <!-- Apex Charts -->
+    <script src="/vendor/apexcharts/apexcharts.min.js"></script>
+
+    <!-- Charts -->
+    <script src="/assets/js/chartist.min.js"></script>
+    <script src="/assets/js/chartist-plugin-tooltip.min.js"></script>
+
+    <!-- Datepicker -->
+    <script src="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/dist/js/datepicker.min.js"></script>
+
+    <!-- Sweet Alerts 2 -->
+    <script src="/assets/js/sweetalert2.all.min.js"></script>
+
+    <!-- Moment JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+
+    <!-- Notyf -->
+    <script src="/vendor/notyf/notyf.min.js"></script>
+
+    <!-- Simplebar -->
+    <script src="/assets/js/simplebar.min.js"></script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    
+    <!-- Volt JS -->
+    <script src="/assets/js/volt.js"></script>
+    <script src="/assets/js/custom-script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+
+
+
+</head>
+
+<body> 
+<main>
+    <title>Volt Laravel Dashboard - Sign In page</title>
+    <!-- Section -->
+    <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
+        <div class="container">
+            {{-- <p class="text-center"><a href="{{ route('dashboard') }}" class="text-gray-700"><i
+                class="fas fa-angle-left me-2"></i> Back to homepage</a></p> --}}
+            <div wire:ignore.self class="row justify-content-center form-bg-image"
+                data-background-lg="http://localhost/mayacomputercentre/assets/img/illustrations/signin.svg">
+                <div class="col-12 d-flex align-items-center justify-content-center">
+                    <div class="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+                        <div class="text-center text-md-center mb-4 mt-md-0">
+                            <h1 class="mb-3 h3">Welcome back</h1>
                             
-                            <div>
-                                <p>Don't have an account ? <a href="auth-register.html" class="fw-medium text-primary"> Signup now </a> </p>
-                                <p>© <script>document.write(new Date().getFullYear())</script> Apprise. Crafted with <i class="mdi mdi-heart text-danger"></i> by OfferPlant</p>
+                        </div>
+                        @if(Session::has('error'))
+                            <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('error') }}</p>
+                        @endif
+                        <form class="mt-4" method="POST">
+                            @csrf
+                            <!-- Form -->
+                            <div class="form-group mb-4">
+                                <label for="email">Registration No</label>
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><svg
+                                            class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
+                                            </path>
+                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                        </svg></span>
+                                    <input  type="text" name="reg_no" class="form-control"
+                                        placeholder="Registration No" id="reg_no" autofocus required>
+                                </div>
+                                @error('email') <div wire:key="form" class="invalid-feedback"> {{$message}} </div>
+                                @enderror
                             </div>
-                        </div> -->
-
+                            <!-- End of Form -->
+                            <div class="form-group">
+                                <!-- Form -->
+                                <div class="form-group mb-4">
+                                    <label for="password">Your Password</label>
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="basic-addon2"><svg
+                                                class="icon icon-xs text-gray-600" fill="currentColor"
+                                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg></span>
+                                        <input name="mobile" type="password" placeholder="Password"
+                                            class="form-control" id="password" required>
+                                    </div>
+                                    @error('password') <div class="invalid-feedback"> {{ $message }} </div> @enderror
+                                </div>
+                                <!-- End of Form -->
+                                
+                            </div>
+                            <div class="d-grid">
+                                <button type="submit" class="btn btn-gray-800">Sign in</button>
+                            </div>
+                        </form>
+                       
                     </div>
                 </div>
             </div>
         </div>
-        <!-- end account-pages -->
+    </section>
+</main>
+</body>
 
-        <!-- JAVASCRIPT -->
-        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
-        
-        <!-- App js -->
-        <script src="{{ asset('backend/assets/js/app.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/jquery-validation/jquery.validate.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/js/notify.min.js') }}"></script>
-        <script>
-        $(document).ready(function() {
-        $(window).keydown(function(event){
-        if(event.keyCode == 13) {
-          event.preventDefault();
-          //return false;
-        $("#login_btn").trigger('click');
-        }
-        });
-        });
-    </script>
-    </body>
 </html>
